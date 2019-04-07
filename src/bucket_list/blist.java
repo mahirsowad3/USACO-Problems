ce28fbb9487e58da5df808c8cc89da7226a04e8b
mahirsowad3 <mahirsowad3@gmail.com>
2019-04-07 16:01:52 -0400

    Timing Out

--- src/bucket_list/blist.java
+++ src/bucket_list/blist.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class blist {
 	public static void main(String[] args) throws IOException {
 		BufferedReader infile = new BufferedReader(new FileReader("blist.in"));
+		PrintWriter outfile = new PrintWriter(new BufferedWriter(new FileWriter("blist.out")));
 		int N = Integer.parseInt(infile.readLine());
 		ArrayList<Integer> sT = new ArrayList<>();
 		ArrayList<Integer> eT = new ArrayList<>();
@@ -38,13 +39,12 @@ public class blist {
 				maxB = curB;
 			}
 		}
-		BufferedWriter outfile = new BufferedWriter(new FileWriter("blist.out"));
-		outfile.write(Integer.toString(maxB));
-		outfile.write("\n");
+		outfile.println(maxB);
 		outfile.close();
+		System.out.println(maxB);
 	}
 	
-	private static boolean between(int start, int end, int num) {
+	public static boolean between(int start, int end, int num) {
 		if (num >= start && num <= end) {
 			return true;
 		} else {
