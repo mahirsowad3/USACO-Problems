f11c47f39d8e8322c4ab9ae877e5a2d84b89fb8a
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-04-12 18:44:40 -0400

    Shell Game completed

--- src/cow_herding/herding.java
+++ src/cow_herding/herding.java
@@ -12,25 +12,39 @@ public class herding {
 		locations.add(Integer.parseInt(s.split(" ")[1]));
 		locations.add(Integer.parseInt(s.split(" ")[2]));
 		Collections.sort(locations);
+		infile.close();
 		
 		int minMoves = 0;
 		int maxMoves = 0;
-		if (Math.abs(locations.get(0) - locations.get(1)) == 1 && Math.abs(locations.get(1)-locations.get(2)) == 1) {
+		
+		if (isConsecutive(locations.get(0), locations.get(1), locations.get(2))) {
 			minMoves = 0;
 			maxMoves = 0;
 		}
-		if (Math.abs(locations.get(0) - locations.get(1)) > 1 && Math.abs(locations.get(1)-locations.get(2)) == 1 || locations.get(0) - locations.get(1) == 1 && Math.abs(locations.get(1)-locations.get(2)) > 1) {
-			minMoves = 1;
-			maxMoves = 2;
-		} 
-		if (Math.abs(locations.get(0) - locations.get(1)) > 1 && Math.abs(locations.get(1)-locations.get(2)) > 1) {
-			minMoves = 2;
-			maxMoves = 2;
+		else {
+			if (locations.get(1) - locations.get(0) == 2 || locations.get(2) - locations.get(1) == 2) {
+				minMoves = 1;
+			} else {
+				minMoves = 2;
+			}
+			//calculating max
+			maxMoves = Math.max(locations.get(1) - locations.get(0), locations.get(2) - locations.get(1)) - 1;
 		}
-		
-		System.out.println(minMoves);
-		System.out.println(maxMoves);
-		
-		
+		PrintWriter outfile = new PrintWriter("herding.out");
+		outfile.println(minMoves);
+		outfile.println(maxMoves);
+		outfile.close();
+	}
+	
+	public static boolean isConsecutive(int a, int b, int c) {
+		if (b-a == 1 && c-b == 1) {
+			return true;
+		} 
+		if ((b-a == 2 && c-b == 1) || (b-a == 1 && c-b == 2)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
