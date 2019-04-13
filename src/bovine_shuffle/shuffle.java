008616e00d42d5a3ae6e6a108021acbae218b832
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-04-13 19:06:55 -0400

    Completed Bovine Shuffle. Passed all test cases.

--- src/bovine_shuffle/shuffle.java
+++ src/bovine_shuffle/shuffle.java
@@ -7,18 +7,35 @@ public class shuffle {
 	public static void main(String[] args) throws IOException {
 		BufferedReader infile = new BufferedReader(new FileReader("shuffle.in"));
 		int N = Integer.parseInt(infile.readLine());
-		HashMap<Integer, Integer> pairs = new HashMap<>();
+		int[] pattern = new int[N];
+		int[] ids = new int[N];
 		String s1 = infile.readLine();
 		String s2 = infile.readLine();
 		for (int i = 0; i < N; i++) {
-			pairs.put(Integer.parseInt(s1.split(" ")[i]), Integer.parseInt(s2.split(" ")[i]));
+			pattern[i] = Integer.parseInt(s1.split(" ")[i]);
+			ids[i] = Integer.parseInt(s2.split(" ")[i]);
 		}
 		
 		infile.close();
+		
+		int[] originalOrder = reverseSwapThreeTimes(pattern, ids);
+		
 		PrintWriter outfile = new PrintWriter("shuffle.out");
-		for (int i = 1; i <= N; i++) {
-			outfile.println(pairs.get(i));
+		for (int i = 0; i < N; i++) {
+			outfile.println(originalOrder[i]);
 		}
 		outfile.close();
+	}
+	
+	public static int[] reverseSwap(int[] pattern, int[] ids) {
+		int[] newOrder = new int[pattern.length];
+		for (int i = 0; i < pattern.length; i++) {
+			newOrder[i] = ids[pattern[i]-1];
+		}
+		return newOrder;
+	}
+	
+	public static int[] reverseSwapThreeTimes(int[] pattern, int[] ids) {
+		return reverseSwap(pattern, reverseSwap(pattern, reverseSwap(pattern, ids)));
 	}
 }
