bc83a6ff1ac99dae17853b8de031a50342ea66ac
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-04-14 15:02:59 -0400

    Works for most test cases except for 2

--- src/milk_measurement/measurement.java
+++ src/milk_measurement/measurement.java
@@ -20,35 +20,81 @@ public class measurement {
 		
 		infile.close();
 		
-		Integer[] orderedDays = days;
-		Arrays.sort(orderedDays);
-		
-		int changes = 0;
+		System.out.println(Arrays.deepToString(orderedLogs(days, logs)));
+		logs = orderedLogs(days, logs);
 		String currCow = " ";
+		int changes = 0;
 		for (int i = 0; i < N; i++) {
-			if (logs[Arrays.asList(days).indexOf(orderedDays[i])][0].equals("Mildred")) {
-				M = newValue(M, logs[i][1]);
-				if (M == maxOfThree(B, E, M) && !currCow.equals('m')) {
-					currCow = "m";
+			if (logs[i][0].equals("Bessie")) {
+				B = newValue(B, logs[i][1]);
+				if (maxOfThree(B, E, M) == -1 && !currCow.equals("bem")) {
+					currCow = "bem";
 					changes += 1;
 				}
+				else if (maxOfThree(B, E, M) == -2 && !currCow.equals("be")) {
+					currCow = "be";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -3 && !currCow.equals("em")) {
+					currCow = "em";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -4 && !currCow.equals("bm")) {
+					currCow = "bm";
+					changes += 1;
+				}
+				else if (B == maxOfThree(B, E, M) && !currCow.equals("b")) {
+					currCow = "b";
+					changes += 1;
+				} 
 			}
-			else if (logs[Arrays.asList(days).indexOf(orderedDays[i])][0].equals("Elsie")) {
+			if (logs[i][0].equals("Elsie")) {
 				E = newValue(E, logs[i][1]);
-				if (E == maxOfThree(B, E, M) && !currCow.equals('e')) {
+				if (maxOfThree(B, E, M) == -1 && !currCow.equals("bem")) {
+					currCow = "bem";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -2 && !currCow.equals("be")) {
+					currCow = "be";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -3 && !currCow.equals("em")) {
+					currCow = "em";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -4 && !currCow.equals("bm")) {
+					currCow = "bm";
+					changes += 1;
+				}
+				else if (E == maxOfThree(B, E, M) && !currCow.equals("e")) {
 					currCow = "e";
 					changes += 1;
 				}
 			}
-			else if (logs[Arrays.asList(days).indexOf(orderedDays[i])][0].equals("Bessie")) {
-				B = newValue(B, logs[i][1]);
-				if (B == maxOfThree(B, E, M) && !currCow.equals('b')) {
-					currCow = "b";
+			if (logs[i][0].equals("Mildred")) {
+				M = newValue(M, logs[i][1]);
+				if (maxOfThree(B, E, M) == -1 && !currCow.equals("bem")) {
+					currCow = "bem";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -2 && !currCow.equals("be")) {
+					currCow = "be";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -3 && !currCow.equals("em")) {
+					currCow = "em";
+					changes += 1;
+				}
+				else if (maxOfThree(B, E, M) == -4 && !currCow.equals("bm")) {
+					currCow = "bm";
+					changes += 1;
+				}
+				else if (M == maxOfThree(B, E, M) && !currCow.equals("m")) {
+					currCow = "m";
 					changes += 1;
 				}
 			}
 		}
-		
 		PrintWriter outfile = new PrintWriter("measurement.out");
 		outfile.println(changes);
 		outfile.close();
@@ -56,27 +102,44 @@ public class measurement {
 	
 	public static int newValue(int g, String s) {
 		if (s.charAt(0) == '+') {
-			g += Integer.parseInt(s.substring(1));
+			g += Integer.parseInt(s.substring(1, s.length()));
 		} else {
-			g -= Integer.parseInt(s.substring(1));
+			g -= Integer.parseInt(s.substring(1, s.length()));
 		}
 		return g;
 	}
 	
 	public static int maxOfThree(int a, int b, int c) {
-		int max = 0;
+		int max = Math.max(Math.max(a,  b), c);
 		if (a == b && b == c) {
 			return -1;
 		}
-		if (a > max) {
-			max = a;
+		else if (a == max && b == max && c != max) {
+			return -2;
 		}
-		if (b > max) {
-			max = b;
+		else if (a != max && b == max & c == max) {
+			return -3;
 		}
-		if (c > max) {
-			max = c;
+		else if(a == max && b != max && c == max) {
+			return -4;
 		}
-		return max;
+		else {
+			return max;
+		}
+	}
+	
+	public static String[][] orderedLogs(Integer[] days, String[][] logs) {
+		String[][] newOrder = new String[days.length][2];
+		int index = 0;
+		for (int i = 0; i <= 100; i++) {
+			for (int j: days) {
+				if (i == j) {
+					newOrder[index][0] = logs[Arrays.asList(days).indexOf(j)][0];
+					newOrder[index][1] = logs[Arrays.asList(days).indexOf(j)][1];
+					index ++;
+				}
+			}
+		}
+		return newOrder;
 	}
 }
