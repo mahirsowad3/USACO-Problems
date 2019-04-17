7c813d58eb956d4b95c9bc1eed08d37db706c5d3
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2019-04-17 14:57:57 -0400

    Worked for all test cases. Forgot to factor in restriction of only rectangular tarps allowed.

--- src/blocked_billboard_II/billboard.java
+++ src/blocked_billboard_II/billboard.java
@@ -19,10 +19,31 @@ public class billboard {
 		int x4 = infile.nextInt();
 		int y4 = infile.nextInt();
 		
-		int minAreaTarp = calculateArea(x1, y1, x2, y2);
-		if (hasOverlap(x1, y1, x2, y2, x3, y3, x4, y4)) {
-			minAreaTarp -= calculateOverlap(x1, y1, x2, y2, x3, y3, x4, y4);
+		int minAreaTarp = 0; //calculateArea(x1, y1, x2, y2);
+		
+		int coveredCorners = 0;
+		if (cornerCovered(x1, y1, x3, y3, x4, y4)) {
+			coveredCorners++;
 		}
+		if (cornerCovered(x1, y2, x3, y3, x4, y4)) {
+			coveredCorners++;
+		}
+		if (cornerCovered(x2, y1, x3, y3, x4, y4)) {
+			coveredCorners++;
+		}
+		if (cornerCovered(x2, y2, x3, y3, x4, y4)) {
+			coveredCorners++;
+		}
+		
+		if (coveredCorners < 2) {
+			minAreaTarp = calculateArea(x1, y1, x2, y2);
+		} else if (coveredCorners == 2 || coveredCorners == 3) {
+			minAreaTarp = calculateArea(x1, y1, x2, y2) - Math.abs(Math.min(x2, x4) - Math.max(x1, x3)) * Math.abs(Math.min(y2, y4) - Math.max(y1, y3));
+		} else {
+			minAreaTarp = 0;
+		}
+		
+		
 		PrintWriter outfile = new PrintWriter("billboard.out");
 		outfile.println(minAreaTarp);
 		outfile.close();
@@ -32,22 +53,16 @@ public class billboard {
 		return Math.abs(x1 - x2) * Math.abs(y1 - y2);
 	}
 	
-	public static int calculateOverlap(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-		return Math.abs(Math.min(x2, x4) - Math.max(x1, x3)) * Math.abs(Math.min(y2, y4) - Math.max(x1, x3)); 
-	}
-	
-	public static boolean hasOverlap(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-		if (!inRange(x1, x2, x3) && !inRange(y1, y2, y3) || !inRange(x1, x2, x4) && !inRange(y1, y2, y4)) {
-			return false;
-		} else if (!inRange(x1, x2, x3) && !inRange(y1, y2, y4) || !inRange(x1, x2, x4) && !inRange(y1, y2, y3)) {
-			return false;
-		} else {
+	public static boolean inRange(int start, int stop, int x) {
+		if (x >= start && x <= stop) {
 			return true;
+		} else {
+			return false;
 		}
 	}
 	
-	public static boolean inRange(int start, int stop, int x) {
-		if (x >= start && x <= stop) {
+	public static boolean cornerCovered(int x, int y, int x3, int y3, int x4, int y4) {
+		if (inRange(x3, x4, x) && inRange(y3, y4, y)) {
 			return true;
 		} else {
 			return false;
