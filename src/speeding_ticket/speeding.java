01053e1d85b3386fb516c9cfc6ddd6b578bd2b60
mahirsowad3 <mahirsowad3@gmail.com>
2019-06-23 12:19:24 -0400

    Finished retrieving input data

--- src/speeding_ticket/speeding.java
+++ src/speeding_ticket/speeding.java
@@ -8,6 +8,18 @@ public class speeding {
 		Scanner infile = new Scanner(new File("speeding.in"));
 		int N = infile.nextInt();
 		int M = infile.nextInt();
+		int [][] roadSegments = new int[N][2];
+		int[][] cowSegments = new int[M][2];
+		
+		for (int i = 0; i < N; i++) {
+			roadSegments[i][0] = infile.nextInt();
+			roadSegments[i][1] = infile.nextInt();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			cowSegments[i][0] = infile.nextInt();
+			cowSegments[i][1] = infile.nextInt();
+		}
 	}
 
 }
