886dfc8c98e0cf602bebdc168be26c55144a8f76
mahirsowad3 <mahirsowad3@gmail.com>
2019-06-23 12:11:39 -0400

    Started speeding ticket problem

---
+++ src/speeding_ticket/speeding.java
@@ -0,0 +1,13 @@
+package speeding_ticket;
+
+import java.util.*;
+import java.io.*;
+
+public class speeding {
+	public static void main(String args[]) throws IOException {
+		Scanner infile = new Scanner(new File("speeding.in"));
+		int N = infile.nextInt();
+		int M = infile.nextInt();
+	}
+
+}
